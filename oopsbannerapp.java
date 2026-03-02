a9d01b7bbacc2a54fedf4322bbdeac370ed0e329
varshanth82 <varshanthreddypokala8@gmail.com>
2026-03-02 13:30:56 +0530

    i have completed uc4

---
+++ oopsbannerapp.java
@@ -0,0 +1,21 @@
+public class oopsbannerapp {
+
+    public static void main(String[] args) {
+
+        String[] banner = {
+
+            String.join("", " *****   ", " *****   ", " *****    ", " **** "),
+            String.join("", "*     * ", "*     * ", "*     *  ", "*    *"),
+            String.join("", "*     * ", "*     * ", "*     *  ", "*     "),
+            String.join("", "*     * ", "*     * ", "*     *   ", " **** "),
+            String.join("", "*     * ", "*     * ", "*     *       ", "*"),
+            String.join("", "*     * ", "*     * ", "*     *  ", "*    *"),
+            String.join("", " *****   ", " *****   ", " *****    ", " **** ")
+        };
+
+        // Enhanced for-loop to print banner
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
